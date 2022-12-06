f5536cc9064b767f8468b81d57c10715eb34e744
valdmity <vlsur@outlook.com>
2022-12-06 15:14:26 +0500

    script fix

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ function getCookie(name) {
 	let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
 	return matches ? decodeURIComponent(matches[1]) : '-1';
 }
-if (getCookie('id') === '-1'){
-    document.cookie = "id=0";
-}
 let url = 'http://192.168.3.5:8080/' + getCookie('id');
 
 fetch(url)
