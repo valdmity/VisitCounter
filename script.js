2f60c09704d8c2ea250717cc217d9296d80cdc78
valdmity <vlsur@outlook.com>
2022-12-22 02:00:48 +0500

    script fix

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function getCookie(name) {
 	let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-	return matches ? decodeURIComponent(matches[1]) : '-1';
-}
-if (getCookie('user') === '-1'){
-    document.cookie = "user=0";
+	return matches ? decodeURIComponent(matches[1]) : '0';
 }
 let url = 'http://192.168.3.5:8080/' + getCookie('id');
 
